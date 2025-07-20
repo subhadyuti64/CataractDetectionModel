94b9c2706dc249d627bd5bd8130a1bab5a2ceee7
subhadyuti64 <subhadyutirath@gmail.com>
2025-07-20 16:04:30 +0530

    App updated

--- predict_app.py
+++ predict_app.py
@@ -38,7 +38,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     if st.button("Predict"):
         with st.spinner("Analyzing..."):
